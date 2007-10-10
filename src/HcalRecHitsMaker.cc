4f07a5f22ec75b1d1b26f2bca2e0b2990c4d59fa
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2007-10-10 12:55:47 +0000

    Back port the modifications made in 17X to improve random reproducibility

--- src/HcalRecHitsMaker.cc
+++ src/HcalRecHitsMaker.cc
@@ -17,6 +17,7 @@
 #include "FastSimulation/Utilities/interface/RandomEngine.h"
 #include "TFile.h"
 #include "TGraph.h"
+#include <fstream>
 
 class RandomEngine;
 
@@ -374,8 +375,8 @@ void HcalRecHitsMaker::noisify()
 void HcalRecHitsMaker::noisifySubdet(std::map<uint32_t,std::pair<float,bool> >& theMap, const std::vector<uint32_t>& thecells, unsigned ncells, double hcalHotFraction_)
 {
 
-  unsigned mean=(unsigned)((double)(ncells-theMap.size())*hcalHotFraction_);
-  unsigned nhcal = (unsigned)(random_->poissonShoot(mean));
+  double mean = (double)(ncells-theMap.size())*hcalHotFraction_;
+  unsigned nhcal = random_->poissonShoot(mean);
   
   unsigned ncell=0;
   unsigned cellindex=0;
